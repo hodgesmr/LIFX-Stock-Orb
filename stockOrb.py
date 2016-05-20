66afbc7ad29bcaba47d19edffa6472b7a10ae86c
Bo Brinkman <brinkmwj@miamioh.edu>
2016-05-20 13:46:10 +0000

    Color calculation done

--- stockOrb.py
+++ stockOrb.py
@@ -1,20 +1,33 @@
 import requests
 import json
 
+#Set up the auth header
 f = open('token.txt', 'r')
 token = f.readline()
-print token
-
 headers = {
     "Authorization": "Bearer %s" % token,
 }
 
+two_std_dev_change = 2.0
+
+pct_change = 0
+
+#Calculate color from pct_change
+how_green = min(max(1.0 + (pct_change/two_std_dev_change), 0), 1.0)
+how_red = min(max(1.0 - (pct_change/two_std_dev_change), 0), 1.0)
+green = int(255.999*how_green)
+red = int(255.999*how_red)
+
+my_color = "#" + hex(red)[2:].zfill(2) + hex(green)[2:].zfill(2) + "00"
+print my_color
+
+#Build the default/common parts of the payload
 payload = {
     'power' : 'on',
-    'color' : 'red',
+    'color' : my_color,
+    'duration' : 5,
 }
 
-print payload
 
 response = requests.put('https://api.lifx.com/v1/lights/all/state', params=payload, headers=headers)
 print response.text
