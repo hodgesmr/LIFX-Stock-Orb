05362991a9d054139a5f53045f434a57a57a050c
Bo Brinkman <dr.bo.brinkman@gmail.com>
2016-05-20 17:17:25 +0000

    Breathe limits and configurable color handling

--- stockOrb.py
+++ stockOrb.py
@@ -6,6 +6,8 @@ from yahoo_finance import Share
 DAILY_STD_DEV = 1.0 #In percent, what is the standard deviation of stock price change?
 CALL_FREQUENCY = 5 #How often this script will be called, in minutes
 STOCK_TO_TRACK = '^gspc'
+GOOD_COLOR = [0, 255, 0]
+BAD_COLOR = [255, 0, 0]
 
 ######################
 #Get the share price and open, calculate percent change from open
@@ -20,17 +22,18 @@ headers = {
     "Authorization": "Bearer %s" % token,
 }
 
+############################
 #Calculate color from pct_change
-how_green = min(max(1.0 + (pct_change/(2*DAILY_STD_DEV)), 0), 1.0)
-how_red = min(max(1.0 - (pct_change/(2*DAILY_STD_DEV)), 0), 1.0)
-green = int(255.999*how_green)
-red = int(255.999*how_red)
+
+#very good gives lerp_amt of 1, very bad gives 0
+lerp_amt = min(max((2*DAILY_STD_DEV + pct_change)/(4*DAILY_STD_DEV), 0), 1)
+lerped_color = [int(lerp_amt*GOOD_COLOR[i] + (1-lerp_amt)*BAD_COLOR[i]) for i in range(3)]
+highlight_color = [int((255+i)/2) for i in lerped_color]
 
 #Main color
-my_color = "#" + hex(red)[2:].zfill(2) + hex(green)[2:].zfill(2) + "00"
+my_color = "#" + ''.join([hex(i)[2:].zfill(2) for i in lerped_color])
 #Color for breathe effect ... brighter version of main color
-emph_color = "#" + hex((red+255)/2)[2:].zfill(2) + hex((green+255)/2)[2:].zfill(2) + "7f"
-
+emph_color = "#" + ''.join([hex(i)[2:].zfill(2) for i in highlight_color])
 
 ###First, set the light color
 #Build the default/common parts of the payload
@@ -45,7 +48,7 @@ response.raise_for_status()
 
 ####Next, if it is a big change, do the breathe effect
 if abs(pct_change) > 2*DAILY_STD_DEV:
-    breathe_rate = 2/(pct_change/DAILY_STD_DEV)
+    breathe_rate = min(max(abs(2/(pct_change/DAILY_STD_DEV)), 1), 7)
 
     #Build the default/common parts of the payload
     payload = {
@@ -53,7 +56,7 @@ if abs(pct_change) > 2*DAILY_STD_DEV:
         'from_color' : my_color,
         'color' : emph_color,
         'period' : breathe_rate,
-        'cycles' : breathe_rate*CALL_FREQUENCY*60,
+        'cycles' : (1/breathe_rate)*CALL_FREQUENCY*60,
         'persist' : 'true'
     }
 
